256ecf94a46d77b1553ad2a34f684aab7a1d9e97
Michael Haerle <michael.haerle01@gmail.com>
2022-07-05 12:26:18 -0500

    adding 3 files

---
+++ limit_exercises.sql
@@ -0,0 +1,23 @@
+-- MySQL provides a way to return only unique results from our queries with the keyword DISTINCT. 
+-- For example, to find all the unique titles within the company, we could run the following query:
+SELECT DISTINCT title FROM titles;
+
+SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
+
+-- Find all previous or current employees hired in the 90s and born on Christmas. 
+-- Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. 
+-- Write a comment in your code that lists the five names of the employees returned.
+
+SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25' ORDER BY hire_date ASC LIMIT 5;
+-- Alselm Cappello, Utz Mandell, Bouchung Schreiter, Baocai Kushner, Petter Stroustrup.
+
+-- Try to think of your results as batches, sets, or pages. 
+-- The first five results are your first page. The five after that would be your second page, etc. 
+-- Update the query to find the tenth page of results.
+-- LIMIT and OFFSET can be used to create multiple pages of data. 
+-- What is the relationship between OFFSET (number of results to skip), LIMIT (number of results per page), and the page number?
+SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25' ORDER BY hire_date ASC LIMIT 5 OFFSET 50;
+-- The relationship between LIMIT, OFFSET , and the page number is a complementary one.
+-- LIMIT will choose how much data you see at one time.
+-- OFFSET will choose where the LIMIT begins.
+-- The page number tells you how far you are into the data set.
